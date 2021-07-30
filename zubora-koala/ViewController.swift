d5a081cbf3b62bcfbb086957bf8360917ffdd19e
KentaroJay <kentaro.jay.takahashi@gmail.com>
2021-07-30 10:27:53 +0900

    制限時間内に収まっているかどうかでボタンの非表示・表示を入れ替える。（アプリ起動時のみ。デーモン化必須）

--- zubora-koala/ViewController.swift
+++ zubora-koala/ViewController.swift
@@ -18,14 +18,26 @@ class ViewController: UIViewController {
         let dt = Date()
         let dateFormatter = DateFormatter()
 
-        dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "yMMMdHms", options: 0, locale: Locale(identifier: "ja_JP"))
+        dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "HH:mm:ss", options: 0, locale: Locale(identifier: "ja_JP"))
 
         print(dateFormatter.string(from: dt))
         print(dt)
         
-        let dtFrom = (timeIntervalSinceNow: )
+        
 
         if true {
+            buttonTimeToGive.isHidden = true
+        }
+
+        let time1 = "\(dateFormatter.string(from: dt))"
+        let start = "13:10:00"
+        let end = "13:30:00"
+
+        if start <= time1 && time1 <= end {
+            print("範囲内です")
+            buttonTimeToGive.isHidden = false
+        } else {
+            print("範囲外です")
             buttonTimeToGive.isHidden = true
         }
         
