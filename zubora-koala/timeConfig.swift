000869a1684966ec16a1fa41ff9db4b816047f24
moriya440 <suguru.m.0923@gmail.com>
2021-07-30 11:53:31 +0900

    textFieldとdatePickerの連携途中

--- zubora-koala/timeConfig.swift
+++ zubora-koala/timeConfig.swift
@@ -18,10 +18,29 @@ class timeConfigController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // 処理
+        //datepicker上のtoolbarのdoneボタン
+        pickerToolBar = UIToolbar()
+        pickerToolBar.sizeToFit()
+        let toolBarBtn = UIBarButtonItem(title: "DONE", style: .plain, target: self, action: "doneBtn")
+        pickerToolBar.items = [toolBarBtn]
+        fieldTimeConf.inputAccessoryView = pickerToolBar
         
     }
     
     
+    @IBAction func textFieldEditing(_ sender: UITextField) {
+        let datePickerView:UIDatePicker = UIDatePicker()
+        datePickerView.datePickerMode = UIDatePicker.Mode.time
+        sender.inputView = datePickerView
+        datePickerView.addTarget(self, action: Selector("datePickerValueChanged:"), for: UIControl.Event.valueChanged)
+            
+    }
+    
+    //datepickerが選択されたらtextfieldに表示
+    func datePickerValueChanged(sender:UIDatePicker) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat  = "yyyy/MM/dd";
+        birthday.text = dateFormatter.stringFromDate(sender.date)
+    }
     
 }
