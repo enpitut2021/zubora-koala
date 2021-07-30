85a91a362829c84aa6c1098f3906446729c331f7
Mizuna <bizbear.mirai@gmail.com>
2021-07-30 15:58:28 +0900

    DatePicker単純になおした

--- zubora-koala/timeConfig.swift
+++ zubora-koala/timeConfig.swift
@@ -9,42 +9,18 @@ import UIKit
 import SwiftUI
 
 class timeConfigController: UIViewController {
-
-    // timeConfig.storyboard
-    @IBOutlet weak var fieldTimeConf: UITextField!
-    var pickerToolBar: UIToolbar!
     
+    // PickerOnly
+    @IBOutlet weak var pickerConf: UIDatePicker!
+    @IBAction func pickerChanged(_ sender: Any) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss"
+        print(formatter.string(from: pickerConf.date))
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //datepicker上のtoolbarのdoneボタン
-        pickerToolBar = UIToolbar()
-        pickerToolBar.sizeToFit()
-        let toolBarBtn = UIBarButtonItem(title: "DONE", style: .plain, target: self, action: #selector(doneBtn))
-        pickerToolBar.items = [toolBarBtn]
-        fieldTimeConf.inputAccessoryView = pickerToolBar
-        
     }
-    
-    
-    @IBAction func textFieldEditing(_ textField: UITextField) {
-        let datePickerView:UIDatePicker = UIDatePicker()
-        datePickerView.datePickerMode = UIDatePicker.Mode.time
-        textField.inputView = datePickerView
-        datePickerView.addTarget(self, action: #selector(datePickerValueChanged(sender:)), for: UIControl.Event.valueChanged)
-            
-    }
-    
-    //datepickerが選択されたらtextfieldに表示
-    @objc func datePickerValueChanged(sender:UIDatePicker) {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat  = "HH:mm";
-        fieldTimeConf.text = dateFormatter.string(from: sender.date)
-    }
-    
-    //toolbarのdoneボタン
-    @objc func doneBtn(){
-        fieldTimeConf.resignFirstResponder()
-    }
+
 }
