aef930588acf8fbd65f7f8e090e5980329c1e38f
Mizuna <bizbear.mirai@gmail.com>
2021-08-02 15:14:05 +0900

    内容整理

--- zubora-koala/ViewController.swift
+++ zubora-koala/ViewController.swift
@@ -47,35 +47,20 @@ class ViewController: UIViewController {
         return view
     }
     
-    private func loadDate(key: String) -> Date {
-        let value = UserDefaults.standard.object(forKey: key)
-        guard let date = value as? Date else {
-            // default return
-            return Date()
-        }
-        return date
-    }
+    var timerMain: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        5秒おきにタイマーテストをします
-        Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(timerTest), userInfo: nil, repeats: true)
-        
-        let now = Date()
-        let dateFormatter = DateFormatter()
+        // timerTest() を5秒おきに呼び出す登録
+        self.timerMain = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(timerMainView), userInfo: nil, repeats: true)
         
 
+        let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "HH:mm:ss", options: 0, locale: Locale(identifier: "ja_JP"))
         
+        // この処理の目的しりたい
         let flag = UserDefaults.standard.bool(forKey: "isWaterGiven")
-        
-        let start = loadDate(key: "start_time")
-        print(start)
-        
-        let end: Date = Date(timeInterval: 60*30, since: start)
-        print(end)
-        
         if flag == true {
             let view = AnimationView()
             if  let path: String = Bundle.main.path(forResource: "koala-smiling", ofType: "json") {
@@ -99,21 +84,6 @@ class ViewController: UIViewController {
                 view.play()
             }
         }
-        
-
-//        時間範囲指定
-        if start <= now && now <= end {
-            print("範囲内です")
-            buttonTimeToGive.isHidden = false
-        } else {
-            print("範囲外です")
-            buttonTimeToGive.isHidden = true
-            if !flag {
-                image = UIImage(named: "angry")
-                koalaImage.image = image
-            }
-        }
-        
     }
 
     @IBAction func clickButton(_ sender: Any) {
@@ -121,11 +91,28 @@ class ViewController: UIViewController {
         self.present(view, animated: true, completion: nil)
     }
     
-    
-    @objc func timerTest() {
-        print("test")
+    @objc func timerMainView() {
+        
+        let flag = UserDefaults.standard.bool(forKey: "isWaterGiven")
+        
+        let now = Date()
+        let start = loadDate(key: "start_time")
+        let end = Date(timeInterval: 60*30, since: start)
+        
+        // 時間範囲指定
+        if start <= now && now <= end {
+            // print("範囲内です")
+            buttonTimeToGive.isHidden = false
+        } else {
+            // print("範囲外です")
+            buttonTimeToGive.isHidden = true
+            if !flag {
+                image = UIImage(named: "angry")
+                koalaImage.image = image
+            }
+        }
+        // print("test")
     }
-
 
 }
 
