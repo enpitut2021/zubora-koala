8124558ca967edd2061640406da88200d8360b9a
KentaroJay <kentaro.jay.takahashi@gmail.com>
2021-08-03 10:56:38 +0900

    アニメーションの切り替えできた。

--- zubora-koala/ViewController.swift
+++ zubora-koala/ViewController.swift
@@ -10,33 +10,18 @@ import SwiftUI
 import Lottie
 
 class ViewController: UIViewController {
-    
-    //@IBOutlet weak var buttonTimeToGive: UIButton!
-    //@IBOutlet weak var koalaImage: UIImageView!
     var image: UIImage!
+    var viewMain: AnimationView = AnimationView()
     
     @IBOutlet weak var plantimg: UIImageView!
     @IBOutlet weak var yukarieatButton: UIButton!
     
     @IBAction func yukariEat(_ sender: Any) {
-        //var flag = UserDefaults.standard.bool(forKey: "isWaterGiven")
-        //var yukariEatingTime: Timer!
-        
-        var view = AnimationView()
-        koalaEating(name: "eat_animation", view: view)
+        koalaEating(name: "eat_animation", view: self.viewMain)
         yukarieatButton.isEnabled = false
         Timer.scheduledTimer(withTimeInterval: 5, repeats: false) {_ in
-            self.koalaEating(name: "koala-smiling", view: view)
-            Timer.scheduledTimer(timeInterval: 0, target: self, selector: #selector(self.plantVis), userInfo: nil, repeats: false)
         }
     }
-    @objc func plantVis() {
-        plantimg.isHidden = true
-    }
-    
-//    @objc private func koalaStopEating(_ view: AnimationView){
-//        view.stop()
-//    }
     
     func koalaEating(name: String, view: AnimationView) -> AnimationView{
         if  let path: String = Bundle.main.path(forResource: name, ofType: "json") {
@@ -52,50 +37,17 @@ class ViewController: UIViewController {
     }
     
     var timerMain: Timer?
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // timerTest() を5秒おきに呼び出す登録
-        self.timerMain = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(timerMainView), userInfo: nil, repeats: true)
-        
-
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = DateFormatter.dateFormat(fromTemplate: "HH:mm:ss", options: 0, locale: Locale(identifier: "ja_JP"))
-        
-        // この処理の目的しりたい
-        let flag = UserDefaults.standard.bool(forKey: "isWaterGiven")
-        if flag == true {
-            let view = AnimationView()
-            if  let path: String = Bundle.main.path(forResource: "koala-smiling", ofType: "json") {
-                //print(path)
-                view.animation = Animation.filepath(path)
-                view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-                view.center = self.view.center
-                view.loopMode = .loop
-                self.view.addSubview(view)
-                view.play()
-            }
-        } else {
-            let view = AnimationView()
-            if  let path: String = Bundle.main.path(forResource: "51431-koala-wink", ofType: "json") {
-                //print(path)
-                view.animation = Animation.filepath(path)
-                view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-                view.center = self.view.center
-                view.loopMode = .loop
-                self.view.addSubview(view)
-                view.play()
-            }
-        }
+        self.timerMain = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(timerMainView(view:)), userInfo: nil, repeats: true)
     }
 
-//    @IBAction func clickButton(_ sender: Any) {
-//        let view = UIHostingController(rootView: ContentView())
-//        self.present(view, animated: true, completion: nil)
-//    }
     
-    @objc func timerMainView() {
+    @objc func timerMainView(view: AnimationView) {
         
         let flag = UserDefaults.standard.bool(forKey: "isWaterGiven")
         
@@ -117,7 +69,30 @@ class ViewController: UIViewController {
                 //koalaImage.image = image
             }
         }
-        // print("test")
+        
+        
+        // この処理の目的しりたい
+        if flag == true {
+            if  let path: String = Bundle.main.path(forResource: "koala-smiling", ofType: "json") {
+                //print(path)
+                self.viewMain.animation = Animation.filepath(path)
+                self.viewMain.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+                self.viewMain.center = self.view.center
+                self.viewMain.loopMode = .loop
+                self.view.addSubview(self.viewMain)
+                self.viewMain.play()
+            }
+        } else {
+            if  let path: String = Bundle.main.path(forResource: "51431-koala-wink", ofType: "json") {
+                //print(path)
+                self.viewMain.animation = Animation.filepath(path)
+                self.viewMain.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+                self.viewMain.center = self.view.center
+                self.viewMain.loopMode = .loop
+                self.view.addSubview(self.viewMain)
+                self.viewMain.play()
+            }
+        }
     }
 
 }
